7df55e3b66dda12b44c72f3869d10654677dd1a7
fernando zaguirre <fernandozaguirre@hotmail.com>
2023-10-11 00:47:46 -0300

    area circulo

--- circunferencia.c
+++ circunferencia.c
@@ -5,6 +5,7 @@ int main(){
     const double PI = 3.15169;
     double radio;
     double circunferencia;
+    double area;
 
     printf("Calcula la circunferencia de un circulo\n");
 
@@ -12,9 +13,10 @@ int main(){
     scanf("%lf", &radio);
 
     circunferencia = 2 * PI * radio;
+    area = PI * radio *radio;
 
-    printf("La circunferencia del circulo radio %.2lf, es: %.2lf",radio,circunferencia);
-
+    printf("La circunferencia del circulo radio %.2lf, es: %.2lf\n",radio,circunferencia);
+    printf("Su area es: %lf", area);
 
 
     return 0;
